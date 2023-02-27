1d1852f0718b0dfe84d8c2086e22e2447833f2c8
Girish Gopaul <girish@gopaul.me>
2023-02-27 18:27:58 +0400

    Add eslint + prettier & fix issues

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'prettier',
+    'react-app',
+    'react-app/jest',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 2,
+    '@typescript-eslint/triple-slash-reference': 'off',
+  },
+}
